00cdfd2217ac889541afe39f037c30cdb5dfed2b
davszs <dssantos4409@gmail.com>
2025-05-05 16:58:56 -0300

    criação das rotas e views

---
+++ resources/views/login.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Google Fonts Poppins -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    
+    <!-- Biblioteca de ícones -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href={{asset('css/login.css')}}>
+    <img src="{{ asset('images/plano_fundo.png') }}">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main class="container">
+        <form action="valida.php" method="POST">
+            <img class="logo" src="Images/logo.png">
+            <h1>Área Exclusiva</h1>
+
+            <div class="input-box">
+                <input type="email" name="email" placeholder="Usuário" required>
+                <i class="bx bxs-user"></i>
+            </div>
+
+            <div class="input-box">
+                <input type="password" name="senha" placeholder="Senha" required>
+                <i class="bx bxs-lock-alt"></i>
+            </div>
+
+            <div class="input-box">
+                <h3>Fazer login como:</h3>
+                <select name="tipo_usuario" required>
+                    <option value="administrador">Administrador</option>
+                    <option value="professor">Professor</option>
+                    <option value="aluno">Aluno</option>
+                </select>
+            </div><br>
+
+            <div class="remember-forgot">
+                <label>
+                    <input type="checkbox">
+                    Lembrar senha
+                </label>
+                <a href="rec_senha.html">Esqueci minha senha</a>
+            </div>
+
+            <button type="submit" class="login">Login</button>
+        </form>
+    </main>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ResetPasswordController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+Route::get('/financeiro', function () {
+    return view('financeiro');
+})->name('financeiro');
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/cadastro', [RegisterController::class, 'cadastro']);
+Route::post('/recuperar-senha', [ResetPasswordController::class, 'recuperar-senha']);
